1721d64a2e9c2d2596444313c387d86bee777abf
ittrainingstu <ittrainingstu@gmail.com>
2013-11-13 09:03:22 +0000

    test

---
+++ h/define.h
@@ -0,0 +1,128 @@
+#ifndef	__DEF__
+#define __DEF__
+/* 振盪器頻率 */
+#define	FOSC	12000000
+/*  自定型態  */
+typedef  unsigned char  uint8;
+typedef  unsigned int   uint16;
+typedef  unsigned short long   uint24;
+typedef  unsigned long  uint32;
+typedef  char	int8;
+typedef  int	int16;
+typedef  short long	int24;
+typedef  long	int32;
+typedef  far rom char*  FRC;
+
+#define	BIT0	1
+#define	BIT1	(1<<1)
+#define	BIT2	(1<<2)
+#define	BIT3	(1<<3)
+#define	BIT4	(1<<4)
+#define	BIT5	(1<<5)
+#define	BIT6	(1<<6)
+#define	BIT7	(1<<7)
+
+#define	SET(reg,b)  reg |= (b)
+#define	CLR(reg,b)  reg &= ~(b)
+#define	TGL(reg,b)  reg ^= (b)
+#define	DIRIN(reg,b)  reg |= (b)
+#define	DIROUT(reg,b)  reg &= ~(b)
+
+struct srtcc {
+	uint8	year;
+	uint8	mon;
+	uint8	day;
+	uint8	hour;
+	uint8	week;
+	uint8	min;
+	uint8	sec;
+};	
+
+/*  結構位元定義  */
+struct _S8 {
+	unsigned b0:1;
+	unsigned b1:1;
+	unsigned b2:1;
+	unsigned b3:1;
+	unsigned b4:1;
+	unsigned b5:1;
+	unsigned b6:1;
+	unsigned b7:1;
+};
+
+#define	B0(x)	((struct _S8 *)&x)->b0
+#define	B1(x)	((struct _S8 *)&x)->b1
+#define	B2(x)	((struct _S8 *)&x)->b2
+#define	B3(x)	((struct _S8 *)&x)->b3
+#define	B4(x)	((struct _S8 *)&x)->b4
+#define	B5(x)	((struct _S8 *)&x)->b5
+#define	B6(x)	((struct _S8 *)&x)->b6
+#define	B7(x)	((struct _S8 *)&x)->b7
+/* 自定位元，定義與使用
+#define	_tmr0F	B0(initF)	//定義 initF 第0位元為 _tmr0F
+	_tmr0F = 1;
+	if (_tmr0F == 0){ _tmr0F = 1; }
+*/
+
+extern near unsigned char	initF,initF1;
+#define	_tmr0F	B0(initF)
+#define	_7ledF	B1(initF)
+#define	_uartF	B2(initF)
+#define	_rtccF	B3(initF)
+#define	_lcdF	B4(initF)
+#define	_rfidF	B5(initF)
+#define	_intF	B6(initF)	//Demo 程式中斷許可
+#define _adcF   B7(initF)
+
+#define _ccrF   B0(initF1)
+
+
+#define	RELAY	B0(LATD)
+#define	BEEP	B1(LATD)
+/***  INTCON  ***/
+#define	_GIEH	B7(INTCON)
+#define	_GIEL	B6(INTCON)
+
+/***  RCON  ***/
+#define	_IPEN	B7(RCON)
+#define INTON() {_IPEN=1; _GIEL=1; _GIEH=1;}
+#define INTOFF() _GIEH=0
+
+//--- PPS input PIN
+#define miINT1   RPINR1
+#define miINT2   RPINR2
+#define miINT3   RPINR3
+#define miT0CKI  RPINR4
+#define miT3CKI  RPINR6
+#define miCCP1   RPINR7
+#define miCCP2   RPINR8
+#define miT1G    RPINR12
+#define miT3G    RPINR13
+#define miURX2   RPINR16
+#define miUCK2   RPINR17
+#define miSDI2   RPINR21
+#define miSCKIN2 RPINR22
+#define miSSIN2  RPINR23
+#define miPWMFL  RPINR24
+//--- PPS RPORx output PIN
+#define moNULL   0
+#define moC1OUT  1
+#define moC2OUT  2
+#define moUTX2   5
+#define moUDT2   6
+#define moSDO2   9       //SPI data out
+#define moSCK2   10      //SPI clock out
+#define moSSDMA  12      //SPI DMA 從選擇
+#define moULPOUT 13      //低功耗喚醒事件
+#define moCCP1   14      //CCP1
+#define moP1B    15
+#define moP1C    16
+#define moP1D    17
+#define moCCP2   18      //CCP2
+#define moP2B    19
+#define moP2C    20
+#define moP2D    21
+
+#define RPO(fn,rpn)  *(&RPOR0+rpn) = fn;
+#define RPI(fn,rpn)  fn=rpn;
+#endif
---
+++ src/rfim.c
@@ -0,0 +1,148 @@
+#include "WengPIC.h"
+#include <stdio.h>
+
+#define     len     rcv[0]
+#define     tp      rcv[1]
+#define     ID      rcv[2]
+#define     _EID    rcv[3]
+#define     _PID    rcv[3]
+#define     _PSTA   rcv[4]
+#define     MyID    11//看PIC板定義
+#define     MID     254
+#define     _askparking(id)       sendpkg(2,id,0)
+#define     _askenter(id)         sendpkg(3,id,0)
+
+
+#define rcv     ccbuf
+
+#if 1
+extern uint8 ccbuf[64];
+//uint8 rcv[64];
+uint8 psta;//parking status
+uint8 esta;
+uint8 cardid[8];
+uint8 retry;
+uint8 pary[32];
+
+
+uint8 sendpkg(uint8 type,uint8 TID,uint8 *data){
+    tp = type ;
+    switch (type){
+        case 1 :
+            len = 2;
+            ID = 0;
+            break;
+        case 2 : 
+        case 3 :
+            len = 2;
+            ID = TID ;
+            break;
+        case 4 :
+            len = 3;
+            ID = MID;
+            _EID = MyID;
+            if(retry){
+                uint8 i;
+                len =11;
+                for(i=0;i<8;i++){
+                    rcv[4+i] = data[i];
+                    }
+                 }
+            break;
+        case 5 :
+            len = 4;
+            ID = MID;
+            _PID = MyID;
+            _PSTA = psta;
+            break;
+        case 6 :
+        case 7 :
+            len = 2;
+            ID = _EID;
+            break;
+ 
+    }
+    ccSendPkg(rcv);
+}
+void reset(void){
+
+}
+
+/*-------------------------------------------------*/
+#define PNO     3 //車位總數
+void query(void){
+    static uint8 qsta=0;
+    switch(qsta){
+        case 0: _askenter(1);       break;
+        case 1: _askparking(11);    break;
+        case 2: _askparking(12);    break;
+        case 3: _askparking(13);    break;
+        case 4: _askparking(14);    break;
+    } 
+    qsta++;
+    if(qsta>PNO){qsta=0;}
+}
+/*------------------------------------------------*/
+
+
+void EchoParking(uint8 _psta){
+    sendpkg(5,MID,_psta);
+}
+uint8 EchoEnter(uint8 *CARDID){
+        uint8 buf[32];
+        sprintf(buf,"%d,%s",MyID,CARDID);
+        sendpkg(4,MID,buf);
+}
+
+
+int rfim(){
+    
+            if((MyID != ID)&&(ID != 0)){ return 1;}//error 1 not me
+    switch(tp){
+        case 1 : //Reset
+            if(len != 2){break;}
+            if(MyID == MID){break;}//MID送的封包不處理
+            reset();    break;
+        case 2 : //ask parking
+            if(len != 2){break;}
+            if(ID == MID){break;}//MID送的封包不處理
+            EchoParking(psta); break;
+        case 3 : //ask enter
+            if(len != 2){break;}
+            if(ID == MID){break;}  //MID送的封包不處理          
+
+           EchoEnter(rfid);break;
+        case 4 : //echo enter
+            if(len == 3 ){break;}
+            if(len == 11){
+                //uprint(s);//UART Printf CARDID
+                sendpkg(7,_EID,0);//ACK
+                break;
+            }
+        case 5 : //echo parking
+            if(len != 4){break;}
+            {uint8 r;
+            r=0;
+            if(pary[_PID-11]=='1'){r=BIT1;}
+            pary[_PID-11]=_PSTA+'0';
+            r+=(pary[_PID-11]=='1');
+            if(r==1){sendpkg(6,_EID,0);}
+            }
+            break;
+        case 6 : //command 命令指令
+            if(len != 2){break;} 
+            esta=1;
+            break;
+            
+        case 7 :  //ack
+            if(len != 2){break;}
+            retry=0;
+            break;
+
+    }
+
+
+}
+
+
+#endif
